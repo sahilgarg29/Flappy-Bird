e95342b86c3c35b110e865b6fe84387c02a4aae1
sahil <gargs2483@gmail.com>
2018-06-11 19:15:47 +0530

    implement the menu state

--- core/src/com/sahil/game/FlappyBird.java
+++ core/src/com/sahil/game/FlappyBird.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sahil.game.states.GameStateManager;
+import com.sahil.game.states.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 
@@ -13,27 +15,29 @@ public class FlappyBird extends ApplicationAdapter {
 
 	public static final String TITLE = "Flappy Bird";
 
-	SpriteBatch batch;
+	private GameStateManager gsm;
+	private SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+		gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
+
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	    gsm.update(Gdx.graphics.getDeltaTime());
+	    gsm.render(batch);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
 }
---
+++ core/src/com/sahil/game/states/MenuState.java
@@ -0,0 +1,35 @@
+package com.sahil.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sahil.game.FlappyBird;
+
+public class MenuState extends State {
+
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0,0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH /2) - (playBtn.getWidth()/2), FlappyBird.HEIGHT/2);
+        sb.end();
+    }
+}
